6811e31bf0052db57c012384311ecb980c09a266
Nurikhsan <nurikhsan.developer@dansmultipro.com>
2023-05-10 09:21:23 +0700

    init

---
+++ src/main/java/belajar/auto/deploy/BelajarAutoDeployApplication.java
@@ -0,0 +1,14 @@
+package belajar.auto.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarAutoDeployApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Work");
+		SpringApplication.run(BelajarAutoDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/belajar/auto/deploy/BelajarAutoDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package belajar.auto.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarAutoDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
